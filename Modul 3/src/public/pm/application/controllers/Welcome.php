9765b9a20f5caedeed15268c3ee81d8b3f246a20
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-05-05 08:10:49 +0700

    add: data mahasiswa

--- Modul 3/src/public/pm/application/controllers/Welcome.php
+++ Modul 3/src/public/pm/application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +19,27 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+	// public function index()
+	// {
+	// 	$this->load->view('welcome_message');
+	// }
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('MahasiswaModel'); // Memuat model MahasiswaModel
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['mahasiswa'] = $this->MahasiswaModel->get_mahasiswa(); // Mendapatkan data mahasiswa dari model
+
+		$keyword = $this->input->get('keyword'); // Ambil kata kunci pencarian dari form
+		if (!empty($keyword)) {
+			$data['mahasiswa'] = $this->MahasiswaModel->search_mahasiswa($keyword); // Cari mahasiswa berdasarkan nama
+		}
+
+		$data['keyword'] = $keyword;
+		$this->load->view('mahasiswa_view', $data); // Menampilkan data mahasiswa ke view
 	}
 }
---
+++ Modul 3/src/public/pm/application/views/mahasiswa_view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Data Mahasiswa</title>
+</head>
+
+<body>
+	<h1>Data Mahasiswa</h1>
+	<form method="get">
+		<input type="text" required name="keyword" placeholder="Cari mahasiswa berdasarkan nama..." value="<?= $keyword ?>">
+		<button type="submit">Cari</button>
+		<button onclick="window.location.href='<?php echo base_url('/'); ?>'" type="button">Reset</button>
+	</form>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Nama</th>
+				<th>NPM</th>
+				<th>Angkatan</th>
+				<th>Kelas</th>
+				<th>Alamat</th>
+				<th>Mata Kuliah Favorit</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($mahasiswa as $mhs) : ?>
+				<tr>
+					<td><?php echo $mhs['id']; ?></td>
+					<td><?php echo $mhs['nama']; ?></td>
+					<td><?php echo $mhs['npm']; ?></td>
+					<td><?php echo $mhs['angkatan']; ?></td>
+					<td><?php echo $mhs['kelas']; ?></td>
+					<td><?php echo $mhs['alamat']; ?></td>
+					<td><?php echo $mhs['mata_kuliah_favorit']; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+</body>
+
+</html>
