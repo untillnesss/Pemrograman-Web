7abfe19b669aaa3a5236b9eb21ab8d3f0cf54f18
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-05-11 19:49:39 +0700

    add: old link

--- Modul 3/src/public/pm/application/controllers/Pm.php
+++ Modul 3/src/public/pm/application/controllers/Pm.php
@@ -6,7 +6,10 @@ class Pm extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		
+	}
+
+	function index()
+	{
 		header('Location: https://said.remot.id/mahasiswa/');
 		exit;
 	}
