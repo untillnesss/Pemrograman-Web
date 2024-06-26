4fa283fb1aa8eeb695182aa4073b940701ac4ab2
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-06-26 21:38:08 +0700

    add: sweetalert

--- Modul 3/src/public/uas/application/controllers/Login.php
+++ Modul 3/src/public/uas/application/controllers/Login.php
@@ -1,96 +1,104 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
-    public function __construct()
+	public function __construct()
 	{
-		parent :: __construct(); 
-			//load the validation library
+		parent::__construct();
+		//load the validation library
 		// $this->load->library('form_validation');
 		// $this->load->library("pagination");
-        // if($this->session->userdata('username') == ''){
-        //     redirect(base_url() . 'login');
+		// if($this->session->userdata('username') == ''){
+		//     redirect(base_url() . 'login');
 		// }
 		$this->load->model("login_model");
-    }
-    
-    public function index(){
-        $this->load->view('.header.php');
-        $this->load->view('login/index');
-        $this->load->view('.footer.php');
-    }
+	}
 
-    public function login_validation(){
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        // $this->load->model('login_model');
+	public function index()
+	{
+		$this->load->view('.header.php');
+		$this->load->view('login/index');
+		$this->load->view('.footer.php');
+	}
 
-        $data = array(
-            'username'=> $username,
-            'password'=> md5($password),
-        );
-        
+	public function login_validation()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		// $this->load->model('login_model');
 
-        $user = $this->login_model->fetch_user($data)->row();
-        if(!empty($user)){
-            if($user->level_user == 1){ 
-                $session_data = array(
-                    'id'=> $user->idtamu,
-                    'username' => $username,
-                    'level_user' => $user->level_user
-                );
-                $this->session->set_userdata($session_data);
-                redirect(base_url() . 'login/enter');
-            }else if($user->level_user == 2){
-                $session_data = array(
-                    'id'=> $user->idtamu,
-                    'username' => $username,
-                    'level_user' => $user->level_user
-                );
-                $this->session->set_userdata($session_data);
-                redirect(base_url() . 'login/enter');
-            }else{
-                redirect(base_url() . 'login');
-            }
-        }else{
-            redirect(base_url() . 'login');
-        }
-    }
+		$data = array(
+			'username' => $username,
+			'password' => md5($password),
+		);
 
-    function enter(){
-        if($this->session->userdata('username') != '' && $this->session->userdata('level_user') == '1'){
-            //echo '<h2>Welcome - ' . $this->session->userdata('username'). '</h2>';
-            //echo '<label><a href="' . base_url() . 'login/logout">Logout</a></label>';
-            redirect(base_url() . 'admin');
-        }else if($this->session->userdata("username") != '' && $this->session->userdata('level_user') == '2'){
-            redirect(base_url() . 'tamu_kamar');
-        }else{
-            redirect(base_url() . 'login');
-        }
-    }
 
-    function registrasi_form(){
-        $this->load->view('.header.php');
-        $this->load->view('login/registrasi');
-        $this->load->view('.footer.php');
-    }
+		$user = $this->login_model->fetch_user($data)->row();
+		if (!empty($user)) {
+			if ($user->level_user == 1) {
+				$session_data = array(
+					'id' => $user->idtamu,
+					'username' => $username,
+					'level_user' => $user->level_user
+				);
+				$this->session->set_userdata($session_data);
+				redirect(base_url() . 'login/enter');
+			} else if ($user->level_user == 2) {
+				$session_data = array(
+					'id' => $user->idtamu,
+					'username' => $username,
+					'level_user' => $user->level_user
+				);
+				$this->session->set_userdata($session_data);
+				redirect(base_url() . 'login/enter');
+			} else {
+				redirect(base_url() . 'login');
+			}
+		} else {
+			$this->session->set_flashdata('error', 'Username atau Password salah, silahkan coba lagi.');
+			redirect(base_url() . 'login');
+		}
+	}
 
-    function registrasi_save(){
-        $data = array(
-            'username'=>$this->input->post('username'),
-            'password'=>$this->input->post('password'),
-            'email'=>$this->input->post('email'),
-            'nama'=>$this->input->post('nama'),
-            'alamat'=>$this->input->post('alamat'),
-            'telepon'=>$this->input->post('notelp'),
-        );
+	function enter()
+	{
+		if ($this->session->userdata('username') != '' && $this->session->userdata('level_user') == '1') {
+			//echo '<h2>Welcome - ' . $this->session->userdata('username'). '</h2>';
+			//echo '<label><a href="' . base_url() . 'login/logout">Logout</a></label>';
+			redirect(base_url());
+		} else if ($this->session->userdata("username") != '' && $this->session->userdata('level_user') == '2') {
+			redirect(base_url() . 'tamu_kamar');
+		} else {
+			redirect(base_url() . 'login');
+		}
+	}
 
-        print_r($data);
-    }
+	function registrasi_form()
+	{
+		$this->load->view('.header.php');
+		$this->load->view('login/registrasi');
+		$this->load->view('.footer.php');
+	}
 
-    function logout(){
-        $this->session->unset_userdata('username');
-        redirect(base_url() . 'login');
-    }
+	function registrasi_save()
+	{
+		$data = array(
+			'username' => $this->input->post('username'),
+			'password' => $this->input->post('password'),
+			'email' => $this->input->post('email'),
+			'nama' => $this->input->post('nama'),
+			'alamat' => $this->input->post('alamat'),
+			'telepon' => $this->input->post('notelp'),
+		);
+
+		print_r($data);
+	}
+
+	function logout()
+	{
+		$this->session->unset_userdata('username');
+		redirect(base_url() . 'login');
+	}
 }
--- Modul 3/src/public/uas/application/views/.footer.php
+++ Modul 3/src/public/uas/application/views/.footer.php
@@ -1,4 +1,52 @@
-  
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script>
+	const buttonDeletes = document.querySelector(".button-delete");
+
+	buttonDeletes.addEventListener("click", buttonDeleteClick, false);
+
+	function buttonDeleteClick(event) {
+		event.preventDefault();
+
+		Swal.fire({
+			title: "Hapus Barang?",
+			text: "Data barang akan terhapus secara permanen!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Ya, hapus!",
+			cancelButtonText: "Batal",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				window.location.href = event.target.href;
+
+				return true;
+			}
+		});
+	}
+</script>
+
+<?php if ($this->session->flashdata('success')) : ?>
+	<script>
+		Swal.fire({
+			title: "Berhasil!",
+			text: "<?php echo $this->session->flashdata('success'); ?>",
+			icon: "success"
+		});
+	</script>
+<?php endif; ?>
+
+<?php if ($this->session->flashdata('error')) : ?>
+	<script>
+		Swal.fire({
+			title: "Gagal!",
+			text: "<?php echo $this->session->flashdata('error'); ?>",
+			icon: "error"
+		});
+	</script>
+<?php endif; ?>
+
 </body>
 
 </html>
