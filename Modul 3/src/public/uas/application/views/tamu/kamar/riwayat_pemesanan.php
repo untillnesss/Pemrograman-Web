5e1ed1a501bd4ed913be465b280e8c5b5d48a9f9
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2024-06-29 14:59:35 +0700

    add: riwayat pemesanan

--- Modul 3/src/public/uas/application/views/tamu/kamar/riwayat_pemesanan.php
+++ Modul 3/src/public/uas/application/views/tamu/kamar/riwayat_pemesanan.php
@@ -1,104 +1,108 @@
-
 <main class="pt-5 mx-lg-5">
-    <div class="container-fluid mt-5">
-        <div class="row">
-        <?php 
-         if($pemesanans->num_rows() > 0){
-            foreach($pemesanans->result() as $pemesanan){
-                $tglpembayaran = date("Y-m-d", strtotime($pemesanan->batasbayar));
-                $jampembayaran = date("H:i:s", strtotime($pemesanan->batasbayar));
-                
-        ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2>Reservasi</h2>
+			</div>
+		</div>
+		<div class="row">
+			<?php
+			if ($pemesanans->num_rows() > 0) {
+				foreach ($pemesanans->result() as $pemesanan) {
+					$tglpembayaran = date("Y-m-d", strtotime($pemesanan->batasbayar));
+					$jampembayaran = date("H:i:s", strtotime($pemesanan->batasbayar));
 
-            <div class="col-4 mt-5">
-                <!-- Card Dark -->
-                <div class="card">
-                    <!-- Card image -->
-                    <div class="view overlay">
-                        <img width="200" height="200" class="card-img-top" src="<?=site_url()?>uploads/admin/kamar/<?=$pemesanan->gambar?>" alt="Card image cap">
-                        <a>
-                        <div class="mask rgba-white-slight"></div>
-                        </a>
-                    </div>
+			?>
 
-                    <!-- Card content -->
-                    <div class="card-body elegant-color white-text rounded-bottom">
-                        <!-- Title -->
-                        <h4 class="card-title text-center text-success"><?=$pemesanan->tipe?></h4>
-                        <p class="text-center text-info">Tanggal Pemesanan : <?=$pemesanan->tglpesan?></p>
-                        <p class="text-center text-info">Atas Nama : <?=$pemesanan->nama?></p>
+					<div class="col-4">
+						<!-- Card Dark -->
+						<div class="card">
+							<!-- Card image -->
+							<div class="view overlay">
+								<div style="
+									width: 100%; 
+									height: 250px; 
+									background-image: url('<?= site_url() ?>uploads/admin/kamar/<?= $pemesanan->gambar ?>');
+									background-position: center;
+									background-size: cover;
+									border-radius: 0.375rem 0.375rem 0 0;
+								"></div>
+							</div>
 
-                        <hr class="hr-light">
-                        <!-- Text -->
-                        <div class="text-center">
-                            <p class="text-center text-info">Tanggal Check-In : <?=$pemesanan->tglmasuk?></p>
-                            <p class="text-center text-info">Tanggal Check-Out : <?=$pemesanan->tglkeluar?></p>
-                            <p class="text-center text-info">Lama Menginap : <?=$pemesanan->lamahari?> Hari</p>
-                            <h5>Total Bayar : Rp. <?=$pemesanan->totalbayar?></h5>
-                            <?php 
-                            
-                                if($pemesanan->status == 'Pending...'){
-                                    $status="<span class='text-warning'>". $pemesanan->status ."</span>";
-                            ?>
+							<!-- Card content -->
+							<div class="card-body elegant-color white-text rounded-bottom">
+								<!-- Title -->
+								<h4 class="card-title text-primary"><?= $pemesanan->tipe ?></h4>
+								<p class="text-helper mb-1">Tanggal Pemesanan : <?= $pemesanan->tglpesan ?></p>
+								<p class="">Atas Nama : <?= $pemesanan->nama ?></p>
 
-                                <p class="text-center">Status Transaksi : <?=$status?>
-                                </p>
+								<hr class="hr-light">
+								<!-- Text -->
+								<div class="">
+									<p class="mb-1">Tanggal Check-In : <?= $pemesanan->tglmasuk ?></p>
+									<p class="mb-1">Tanggal Check-Out : <?= $pemesanan->tglkeluar ?></p>
+									<p class="mb-4">Lama Menginap : <?= $pemesanan->lamahari ?> Hari</p>
+									<h4 class="mb-3">Total Bayar <?= rupiah($pemesanan->totalbayar) ?></h4>
+									<?php
 
-                                <p class="text-center text-warning">Segera Lakukan Pembayaran Sebelum : <br />
-                                Tanggal : <?=$tglpembayaran?>
-                                Jam : <?=$jampembayaran?>
-                                </p>
-                            
-                                <!-- Link -->                      
-                                <a href="<?=site_url()?>tamu_kamar/pembayaran_form/<?=$pemesanan->idpesan?>" class="btn btn-info">Pembayaran</a>
-                            <?php
-                                }else if($pemesanan->status == 'Konfirmasi'){
-                                $status="<span class='text-default'>Menggunggu ". $pemesanan->status ."</span>";
-                            ?>
+									if ($pemesanan->status == 'Pending...') {
+										$status = "<span class='badge bg-warning'>" . $pemesanan->status . "</span>";
+									?>
 
-                                <p class="text-center">Status Transaksi : <?=$status?>
-                                </p>
+										<div class="alert alert-warning">
+											<p class="mb-0">Belum Bayar</p>
+										</div>
+										<a href="<?= site_url() ?>tamu_kamar/pembayaran_form/<?= $pemesanan->idpesan ?>" class="btn btn-primary">BAYAR</a>
+									<?php
+									} else if ($pemesanan->status == 'Konfirmasi') {
+										$status = "<span class='text-default'>Menggunggu " . $pemesanan->status . "</span>";
+									?>
 
-                            <?php
-                                    
-                                }else if($pemesanan->status == 'Dibatalkan'){
-                                    $status="<span class='text-danger'>". $pemesanan->status ."</span>";
-                            ?>
+										<div class="alert alert-info">
+											<p class="mb-0">Menunggu Konfirmasi</p>
+										</div>
 
-                                <p class="text-center">Status Transaksi : <?=$status?>
-                                </p>
-                            <?php
+									<?php
 
-                                }else if($pemesanan->status == 'Berhasil'){
-                                    $status="<span class='text-success'>". $pemesanan->status ."</span>";
-                            ?>
+									} else if ($pemesanan->status == 'Dibatalkan') {
+										$status = "<span class='text-danger'>" . $pemesanan->status . "</span>";
+									?>
 
-                                <p class="text-center">Status Transaksi : <?=$status?>
-                                </p>
-                            <?php
+										<div class="alert alert-danger">
+											<p class="mb-0">Dibatalkan</p>
+										</div>
+									<?php
 
-                                }else{
-                            ?>
+									} else if ($pemesanan->status == 'Berhasil') {
+										$status = "<span class='text-success'>" . $pemesanan->status . "</span>";
+									?>
+										<div class="alert alert-success">
+											<p class="mb-0">Diterima</p>
+										</div>
+									<?php
 
-                                <p class="text-center">ERROR <?=$status?>
-                                </p>
-                            <?php
-                                }
-                            
-                            ?>
-                        </div>
+									} else {
+									?>
 
-                    </div>
+										<div class="alert alert-light">
+											<p class="mb-0">Tidak diketahui</p>
+										</div>
+									<?php
+									}
 
-                </div>
-            </div>
-        
-        <?php
-            }
-        }
-        ?>
-           
-        </div>
-        
-    </div>
+									?>
+								</div>
+
+							</div>
+
+						</div>
+					</div>
+
+			<?php
+				}
+			}
+			?>
+
+		</div>
+	</div>
 </main>
